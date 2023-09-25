1a2be1bfde63fd55d8f63d3df17ece529eb4ec36
Mary <76174247+maryhbb@users.noreply.github.com>
2023-09-25 19:48:13 +0200

    Implement backend-api-routes/random-character

--- backend-api-routes_random-character/pages/index.js
+++ backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,20 @@
+import useSWR from "swr";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <>
+      <h2>
+        {data.firstName} {data.lastName}
+      </h2>
+
+      <h3>Twitter: {data.twitter}</h3>
+      <h3>Geohash: {data.geohash}</h3>
+    </>
+  );
 }
