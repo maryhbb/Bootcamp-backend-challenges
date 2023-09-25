86dae7f241a59526849c3816bd05008deae3340f
Mary <76174247+maryhbb@users.noreply.github.com>
2023-09-25 18:02:58 +0200

    Implement backend-basics/chance

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,19 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    const name = chance.name();
+    const age = chance.age();
+    const profession = chance.profession({ rank: true });
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Page not found");
+  }
+});
